4bbe98e6f40a720574c9aff2cf3c79ed584356e5
pallavikyama <kyamap98@gmail.com>
2020-12-27 14:01:17 +0530

    Modified: Tested GET, POST, PUT methods using curl and successfully got the desired result.

--- src/main/java/com/firstspringapp/controller/HelloWorldController.java
+++ src/main/java/com/firstspringapp/controller/HelloWorldController.java
@@ -15,28 +15,33 @@ import com.firstspringapp.model.User;
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
+	// curl -X GET "http://localhost:8080/hello" -w "\n"
 	@RequestMapping(value = { "", "/", "home" })
 	public String sayHello() {
-		return "Hello World!!!";
+		return "Hello World!!! Welcome to home page.";
 	}
 
+	// curl -X GET "http://localhost:8080/hello/query/?name=Pallavi" -w "\n"
 	@RequestMapping(value = { "/query" }, method = RequestMethod.GET)
 	public String sayHello(@RequestParam(value = "name") String name) {
-		return "Hello " + name + "!";
+		return "Hello " + name + "! Inside GET method query.";
 	}
 
+	// curl -X GET "http://localhost:8080/hello/param/Pallavi" -w "\n"
 	@GetMapping("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
-		return "Hello " + name + "!";
+		return "Hello " + name + "! Inside get-mapping.";
 	}
 
+	// curl -X POST -H "Content-Type: application/json" -d '{"firstName": "Pallavi", "lastName": "Kyama"}' "http://localhost:8080/hello/post" -w "\n"
 	@PostMapping("/post")
 	public String sayHello(@RequestBody User user) {
-		return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + "! Inside post-mapping.";
 	}
 	
+	// curl -X PUT "http://localhost:8080/hello/put/Pallavi?lastName=Kyama" -w "\n"
 	@PutMapping("/put/{firstName}")
 	public String sayHello(@PathVariable String firstName,@RequestParam(value = "lastName") String lastName) {
-		return "Hello " + firstName + " " + lastName + "!";
+		return "Hello " + firstName + " " + lastName + "! Inside put-mapping.";
 	}
 }
